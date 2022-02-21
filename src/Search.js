7be41cff6714cd7a3f9bae2741ab544dbad29b60
L-delara <92549675+L-delara@users.noreply.github.com>
2022-02-20 19:57:24 -0500

    imported react & axios, created search component

---
+++ src/Search.js
@@ -0,0 +1,58 @@
+import React, { useState } from "react";
+import axios from "axios";
+
+export default function Search() {
+  const [city, setCity] = useState("");
+  const [loaded, setLoaded] = useState(false);
+  const [weather, setWeather] = useState({});
+
+  function showTemp(response) {
+    setLoaded(true);
+    setWeather({
+      cityName: response.data.name,
+      temperature: response.data.main.temp,
+      wind: response.data.wind.speed,
+      humidity: response.data.main.humidity,
+      icon: `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`,
+      description: response.data.weather[0].description,
+    });
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    let apiKey = "4c9b53e4f8f5eb00df5915bdca340605";
+    let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    axios.get(url).then(showTemp);
+  }
+
+  function updateCity(event) {
+    setCity(event.target.value);
+  }
+
+  let form = (
+    <form onSubmit={handleSubmit}>
+      <input type="text" placeholder="type a city" onChange={updateCity} />
+      <input type="submit" value="search" />
+    </form>
+  );
+
+  if (loaded) {
+    return (
+      <div>
+        {form}
+        <h3>{weather.cityName}</h3>
+        <ul>
+          <li>Temperature: {Math.round(weather.temperature)}°C</li>
+          <li>Description: {weather.description}</li>
+          <li>Humidity: {weather.humidity}%</li>
+          <li>Wind: {Math.round(weather.wind)}km/h</li>
+          <li>
+            <img src={weather.icon} alt={weather.description} />
+          </li>
+        </ul>
+      </div>
+    );
+  } else {
+    return form;
+  }
+}
