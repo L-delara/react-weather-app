13adcf0f97b531f5c5ede54762cd1f67032cd37e
L-delara <92549675+L-delara@users.noreply.github.com>
2022-02-20 20:07:48 -0500

    added github link to code

--- src/App.js
+++ src/App.js
@@ -5,6 +5,12 @@ function App() {
   return (
     <div className="App">
       <Search />
+      <br />
+      An{" "}
+      <a href="https://github.com/L-delara/react-weather-app.git">
+        Open-source code
+      </a>{" "}
+      by Ligia De Lara. 😊
     </div>
   );
 }
